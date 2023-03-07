5e38355ce63ada99f9d99d341df24fa4075497ef
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-07 17:16:20 -0600

    Implement remaining_minutes_in_oven

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -3,9 +3,12 @@ pub fn expected_minutes_in_oven() -> i32 {
 }
 
 pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate remaining minutes in oven given actual minutes in oven: {actual_minutes_in_oven}"
-    )
+    let expected = expected_minutes_in_oven();
+    if expected > actual_minutes_in_oven {
+        expected - actual_minutes_in_oven
+    } else {
+        0
+    }
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
