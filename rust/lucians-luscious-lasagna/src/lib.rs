e8fe89db85d14539d20fc0668838dc7d2e63a305
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-07 17:17:28 -0600

    Implement elapsed_time_in_minutes

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -16,7 +16,5 @@ pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate elapsed time in minutes for number of layers {number_of_layers} and actual minutes in oven {actual_minutes_in_oven}"
-    )
+    actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
 }
