cd78ba6071bcfde523e1b2902b973d508741b108
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-07 19:37:35 -0600

    Add missing case

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -5,6 +5,7 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
         1..=4 => CARS_PER_HOUR * (speed as f64),
         5..=8 => CARS_PER_HOUR * (speed as f64) * 0.9,
         9..=10 => CARS_PER_HOUR * (speed as f64) * 0.77,
+        _ => 0.0,
     }
 }
 
