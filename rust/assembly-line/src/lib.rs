3844b2f138a417b69e4527f6522ef28b3cf2a1ab
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-07 19:37:02 -0600

    Implement production_rate_per_hour

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,5 +1,11 @@
+const CARS_PER_HOUR: f64 = 221.0;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    match speed {
+        1..=4 => CARS_PER_HOUR * (speed as f64),
+        5..=8 => CARS_PER_HOUR * (speed as f64) * 0.9,
+        9..=10 => CARS_PER_HOUR * (speed as f64) * 0.77,
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
