e4aa650d253036936366018dd3640a6995eb7834
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-07 19:48:36 -0600

    Implement working_items_per_minute

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -10,5 +10,5 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
