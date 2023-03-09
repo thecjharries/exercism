74e277727104381af4022954c0608d446c6fa500
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-09 17:50:36 -0600

    Refactor to use IntEnum

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,7 +1,8 @@
 use enum_iterator::{all, Sequence};
-use std::fmt;
+use int_enum::IntEnum;
 
-#[derive(Debug, PartialEq, Eq, Sequence, Ord)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Sequence, Ord, IntEnum)]
+#[repr(u32)]
 pub enum ResistorColor {
     Black = 0,
     Blue = 6,
@@ -13,24 +14,6 @@ pub enum ResistorColor {
     Violet = 7,
     White = 9,
     Yellow = 4,
-}
-
-impl fmt::Display for ResistorColor {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let color = match self {
-            ResistorColor::Black => "Black",
-            ResistorColor::Blue => "Blue",
-            ResistorColor::Brown => "Brown",
-            ResistorColor::Green => "Green",
-            ResistorColor::Grey => "Grey",
-            ResistorColor::Orange => "Orange",
-            ResistorColor::Red => "Red",
-            ResistorColor::Violet => "Violet",
-            ResistorColor::White => "White",
-            ResistorColor::Yellow => "Yellow",
-        };
-        write!(f, "{}", color)
-    }
 }
 
 impl PartialOrd for ResistorColor {
@@ -46,20 +29,10 @@ pub fn color_to_value(color: ResistorColor) -> u32 {
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    let color = match value {
-        0 => ResistorColor::Black,
-        1 => ResistorColor::Brown,
-        2 => ResistorColor::Red,
-        3 => ResistorColor::Orange,
-        4 => ResistorColor::Yellow,
-        5 => ResistorColor::Green,
-        6 => ResistorColor::Blue,
-        7 => ResistorColor::Violet,
-        8 => ResistorColor::Grey,
-        9 => ResistorColor::White,
-        _ => return String::from("value out of range"),
-    };
-    color.to_string()
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string(),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
