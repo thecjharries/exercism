49a1143f933a13d39089e15fd58035a76ef145e8
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-11 10:32:21 -0600

    Implement School::grade

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -27,6 +27,8 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!("Return the list of students in {grade}")
+        let mut students = self.grades.get(&grade).unwrap_or(&vec![]).clone();
+        students.sort();
+        students
     }
 }
