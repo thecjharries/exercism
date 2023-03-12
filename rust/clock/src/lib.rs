df62e418f628d1228a2abcc0aab91f50c3f56fae
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-12 11:40:22 -0500

    Refactor Clock::new to use total minutes

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -14,12 +14,13 @@ impl fmt::Display for Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock_minutes = minutes % 60;
-        let clock_hours = (hours + minutes / 60) % 24;
-        Clock {
-            hours: clock_hours,
-            minutes: clock_minutes,
+        let mut total_minutes = hours * 60 + minutes;
+        while total_minutes < 0 {
+            total_minutes += 24 * 60;
         }
+        let hours = total_minutes / 60 % 24;
+        let minutes = total_minutes % 60;
+        Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
