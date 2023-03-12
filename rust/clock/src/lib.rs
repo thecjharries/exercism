fa237e7384d377a89be29663fe4002fe9a563e39
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-12 11:29:27 -0500

    Implement Clock::add_minutes

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -23,6 +23,6 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {minutes} minutes to existing Clock time");
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
