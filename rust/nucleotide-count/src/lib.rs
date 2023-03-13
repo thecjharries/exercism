b21fd53ae04da922990967e57666a10c92174ed5
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-13 18:07:10 -0500

    Implement count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,9 +1,22 @@
 use std::collections::HashMap;
 
+const VALID_NUCLEOTIDES: [char; 4] = ['A', 'C', 'G', 'T'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{nucleotide}' is contained inside DNA string '{dna}'?"
-    );
+    if !VALID_NUCLEOTIDES.contains(&nucleotide) {
+        return Err(nucleotide);
+    }
+
+    let mut count = 0;
+    for c in dna.chars() {
+        if !VALID_NUCLEOTIDES.contains(&c) {
+            return Err(c);
+        }
+        if c == nucleotide {
+            count += 1;
+        }
+    }
+    Ok(count)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
