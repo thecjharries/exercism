c37915d944294cdcd80a53fb5722b992ea74ea4b
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-13 18:08:00 -0500

    Implement nucleotide_counts

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -20,5 +20,16 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!("How much of every nucleotide type is contained inside DNA string '{dna}'?");
+    let mut counts = HashMap::new();
+    for n in VALID_NUCLEOTIDES.iter() {
+        counts.insert(*n, 0);
+    }
+
+    for c in dna.chars() {
+        if !VALID_NUCLEOTIDES.contains(&c) {
+            return Err(c);
+        }
+        *counts.get_mut(&c).unwrap() += 1;
+    }
+    Ok(counts)
 }
