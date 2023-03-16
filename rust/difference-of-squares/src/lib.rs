cd7c940dd8c2868a71fde73db102558b96c054a7
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-16 17:38:55 -0500

    Implement sum_of_squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -3,7 +3,7 @@ pub fn square_of_sum(n: u32) -> u32 {
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
