39f014a18fc1f386e2b318e594ce6ae680c86452
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-15 20:20:15 -0500

    Add range panic

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {s}");
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
 }
 
 pub fn total() -> u64 {
