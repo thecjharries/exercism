be8df72e2ddc6a138eeb45e7d5e2bcd6c87a2bec
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-16 17:49:31 -0500

    Optimize factorization

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,13 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut prime_factors = Vec::new();
     let mut n = n;
-    let mut i = 2;
+    let mut i = 3;
+    while n % 2 == 0 {
+        prime_factors.push(2);
+        n /= 2;
+    }
     while i <= n {
         if n % i == 0 {
             prime_factors.push(i);
             n /= i;
         } else {
-            i += 1;
+            i += 2;
         }
     }
     prime_factors
