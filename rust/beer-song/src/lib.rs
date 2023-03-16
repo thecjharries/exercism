923f510ce69b1f4f2849b0d2a80214be39b73083
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-16 17:32:24 -0500

    Implement verse

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,5 +1,12 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {n}")
+    if 0 == n {
+        return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string();
+    }
+    if 1 == n {
+        return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string();
+    }
+    format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle{} of beer on the wall.\n",
+        n, n, n - 1, if n - 1 == 1 { "" } else { "s" })
 }
 
 pub fn sing(start: u32, end: u32) -> String {
