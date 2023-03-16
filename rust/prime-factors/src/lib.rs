0b55cec0f27328207df092090ffc4ce71392ab5f
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-16 17:48:10 -0500

    Implement factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {n}")
+    let mut prime_factors = Vec::new();
+    let mut n = n;
+    let mut i = 2;
+    while i <= n {
+        if n % i == 0 {
+            prime_factors.push(i);
+            n /= i;
+        } else {
+            i += 1;
+        }
+    }
+    prime_factors
 }
