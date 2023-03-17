c634d2d6e32dab1490eac2edbd372aef53e03ffb
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-17 17:42:18 -0500

    Implement is_leap_year

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
