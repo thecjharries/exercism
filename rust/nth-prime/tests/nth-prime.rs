1c13cceac30d349ca761e4f49524da880e36509a
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-17 17:51:20 -0500

    Test is_prime

--- rust/nth-prime/tests/nth-prime.rs
+++ rust/nth-prime/tests/nth-prime.rs
@@ -19,3 +19,12 @@ fn test_sixth_prime() {
 fn test_big_prime() {
     assert_eq!(np::nth(10_000), 104_743);
 }
+
+#[test]
+fn test_is_prime() {
+    assert!(!np::is_prime(1));
+    assert!(np::is_prime(2));
+    assert!(np::is_prime(3));
+    assert!(np::is_prime(5));
+    assert!(!np::is_prime(8));
+}
