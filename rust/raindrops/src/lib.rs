97bbbfc507cc6169c2af94e7bf07baa41152d27a
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-18 11:49:28 -0500

    Implement raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{n} make?")
+    let mut output = String::new();
+    if n % 3 == 0 {
+        output.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        output.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        output.push_str("Plong");
+    }
+    if output.is_empty() {
+        output.push_str(&n.to_string());
+    }
+    output
 }
