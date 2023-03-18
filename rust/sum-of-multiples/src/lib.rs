0926fcb9677b1357d2cf5adbc30fa4de8e513e40
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-18 11:52:36 -0500

    Implement sum_of_multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut multiples = Vec::new();
+    for factor in factors {
+        if *factor == 0 {
+            continue;
+        }
+        let mut multiple = *factor;
+        while multiple < limit {
+            multiples.push(multiple);
+            multiple += factor;
+        }
+    }
+    multiples.sort();
+    multiples.dedup();
+    multiples.iter().sum()
 }
