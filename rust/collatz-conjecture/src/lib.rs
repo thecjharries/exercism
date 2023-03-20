9704fd6a1bb5fe5c1fd605d7b8f40d87ddce6e5f
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-20 17:51:09 -0500

    Implement collatz

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,5 +1,24 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+    let mut n = n;
+    let mut steps = 0;
+    loop {
+        if n == 1 {
+            return Some(steps);
+        }
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = match n.checked_mul(3) {
+                Some(n) => match n.checked_add(1) {
+                    Some(n) => n,
+                    None => return None,
+                },
+                None => return None,
+            };
+        }
+        steps += 1;
+        if steps > 1000 {
+            return None;
+        }
+    }
 }
