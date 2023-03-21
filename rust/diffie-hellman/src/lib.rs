2f2486582c268f6b36f8eed810db31a00c94bd46
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-21 17:38:35 -0500

    Create mod_exp fnc for modular exponentiation

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,3 +1,18 @@
+pub fn mod_exp(base: u64, exp: u64, modulus: u64) -> u64 {
+    let mut base = base as u128;
+    let mut exp = exp as u128;
+    let modulus = modulus as u128;
+    let mut result = 1 as u128;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exp = exp >> 1;
+        base = (base * base) % modulus;
+    }
+    result as u64
+}
+
 pub fn private_key(p: u64) -> u64 {
     p / 2
 }
