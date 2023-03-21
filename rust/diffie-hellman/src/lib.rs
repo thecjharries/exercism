81ad758a0030a1f92265c351d576d25c3bb47199
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-21 17:38:46 -0500

    Implement public_key

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -18,7 +18,7 @@ pub fn private_key(p: u64) -> u64 {
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+    mod_exp(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
