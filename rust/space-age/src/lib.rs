9cf210bddd629bd21437a3ac728ffbae861e4dae
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-23 16:57:32 -0500

    Implement constants

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -18,13 +18,22 @@ pub trait Planet {
 }
 
 pub struct Mercury;
+const MERCURY_YEAR: f64 = 0.2408467;
 pub struct Venus;
+const VENUS_YEAR: f64 = 0.61519726;
 pub struct Earth;
+const EARTH_YEAR: f64 = 1.0;
+const EARTH_YEAR_IN_SECONDS: f64 = 31557600.0;
 pub struct Mars;
+const MARS_YEAR: f64 = 1.8808158;
 pub struct Jupiter;
+const JUPITER_YEAR: f64 = 11.862615;
 pub struct Saturn;
+const SATURN_YEAR: f64 = 29.447498;
 pub struct Uranus;
+const URANUS_YEAR: f64 = 84.016846;
 pub struct Neptune;
+const NEPTUNE_YEAR: f64 = 164.79132;
 
 impl Planet for Mercury {}
 impl Planet for Venus {}
