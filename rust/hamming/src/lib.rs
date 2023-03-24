ae1fac2545d94d8f1677c4519eaa4f0f2245e387
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-24 17:59:35 -0500

    Implement hamming

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,14 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {s1} and {s2}");
+    if s1.len() != s2.len() {
+        return None;
+    }
+    let mut distance = 0;
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 != c2 {
+            distance += 1;
+        }
+    }
+    Some(distance)
 }
