2a5d0cf3e10f7d8d3ec7f6ff89ba851c0d9538af
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-25 10:20:28 -0500

    Implement find

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,5 +1,17 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+    let mut low = 0;
+    let mut high = array.len();
+    while low < high {
+        let mid = (low + high) / 2;
+        if array[mid] < key {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    if low < array.len() && array[low] == key {
+        Some(low)
+    } else {
+        None
+    }
 }
