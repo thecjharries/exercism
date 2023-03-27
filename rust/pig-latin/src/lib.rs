264d69c60ad9295c44d7cdeeb6ba5a725d259c27
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-27 18:59:43 -0500

    Implement translate

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -1,5 +1,42 @@
+fn is_vowel(c: char) -> bool {
+    match c {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'y' => true,
+        _ => false,
+    }
+}
+
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{input}'"
-    );
+    let exploded = input.split_whitespace().collect::<Vec<&str>>();
+    if 1 < exploded.len() {
+        let mut result = String::new();
+        for word in exploded {
+            result.push_str(&translate(word));
+            result.push(' ');
+        }
+        return result.trim().to_string();
+    }
+    if input.starts_with("xr") || input.starts_with("yt") {
+        return format!("{}ay", input);
+    }
+    if input.starts_with("y") {
+        return format!("{}yay", &input[1..]);
+    }
+    let mut is_q = false;
+    let mut index = 0;
+    for (i, c) in input.chars().enumerate() {
+        if 'q' == c {
+            is_q = true;
+        }
+        if is_vowel(c) {
+            if i == 0 {
+                return format!("{}ay", input);
+            }
+            if is_q && 'u' == c {
+                return format!("{}{}ay", &input[i + 1..], &input[..i + 1]);
+            }
+            index = i;
+            break;
+        }
+    }
+    format!("{}{}ay", &input[index..], &input[..index])
 }
