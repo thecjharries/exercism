5da9996155d1d98489382769aaae373642a6c28a
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-28 18:49:53 -0500

    Add extra modules

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -6,4 +6,26 @@ pub mod graph {
             Graph {}
         }
     }
+
+    pub mod graph_items {
+        pub mod edge {
+            pub struct Edge;
+
+            impl Edge {
+                pub fn new(a: &str, b: &str) -> Self {
+                    Edge {}
+                }
+            }
+        }
+
+        pub mod node {
+            pub struct Node;
+
+            impl Node {
+                pub fn new(a: &str) -> Self {
+                    Node {}
+                }
+            }
+        }
+    }
 }
