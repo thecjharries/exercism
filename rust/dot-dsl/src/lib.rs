f60aecd6f9e924cc1a5da792da62e42b83ab5fe2
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-28 19:00:39 -0500

    Add more struct defn

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,44 +1,58 @@
-use std::collections::HashMap;
-
 pub mod graph {
+    use std::collections::HashMap;
+
     #[derive(Debug, PartialEq)]
     pub struct Graph {
-        nodes: Vec<graph_items::node::Node>,
-        edges: Vec<graph_items::edge::Edge>,
-        attrs: HashMap<&'static str, &'static str>,
-    };
+        pub nodes: Vec<graph_items::node::Node>,
+        pub edges: Vec<graph_items::edge::Edge>,
+        pub attrs: HashMap<&'static str, &'static str>,
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            Graph {}
+            Graph {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+                attrs: HashMap::new(),
+            }
         }
     }
 
     pub mod graph_items {
         pub mod edge {
+            use std::collections::HashMap;
+
             #[derive(Debug, PartialEq)]
             pub struct Edge {
                 pair: (String, String),
                 attrs: HashMap<&'static str, &'static str>,
-            };
+            }
 
             impl Edge {
                 pub fn new(a: &str, b: &str) -> Self {
-                    Edge {}
+                    Edge {
+                        pair: (a.to_string(), b.to_string()),
+                        attrs: HashMap::new(),
+                    }
                 }
             }
         }
 
         pub mod node {
+            use std::collections::HashMap;
+
             #[derive(Debug, PartialEq)]
             pub struct Node {
                 title: String,
                 attrs: HashMap<&'static str, &'static str>,
-            };
+            }
 
             impl Node {
                 pub fn new(a: &str) -> Self {
-                    Node {}
+                    Node {
+                        title: a.to_string(),
+                        attrs: HashMap::new(),
+                    }
                 }
             }
         }
