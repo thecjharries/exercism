f21a29114d5ca1039246cb1287e218e953ec1d03
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-30 17:53:26 -0500

    Define Teams struct

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,3 +1,13 @@
+#[derive(Debug, PartialEq)]
+struct Team {
+    name: String,
+    matches_played: u32,
+    wins: u32,
+    draws: u32,
+    losses: u32,
+    points: u32,
+}
+
 pub fn tally(match_results: &str) -> String {
     unimplemented!(
         "Given the result of the played matches '{match_results}' return a properly formatted tally table string."
