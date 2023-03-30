40b7dbc20bc60d669efe8af4bf924a6a24d302ec
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-30 17:54:42 -0500

    Define string repr for Team

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 struct Team {
     name: String,
@@ -6,6 +8,12 @@ struct Team {
     draws: u32,
     losses: u32,
     points: u32,
+}
+
+impl Display for Team {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:30} | {:2} | {:2} | {:2} | {:2} | {:2}", self.name, self.matches_played, self.wins, self.draws, self.losses, self.points)
+    }
 }
 
 pub fn tally(match_results: &str) -> String {
