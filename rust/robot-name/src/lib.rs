6e6362ac94c2f21ddb9b82c462f4297c2c130afb
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-31 18:24:39 -0500

    Define Robot::new

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,9 +1,19 @@
+use rand::{thread_rng, Rng};
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct Robot(String);
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        let mut name = String::new();
+        let mut rng = thread_rng();
+        for _ in 0..2 {
+            name.push(rng.gen_range(b'A'..b'Z' + 1) as char);
+        }
+        for _ in 0..3 {
+            name.push(rng.gen_range(b'0'..b'9' + 1) as char);
+        }
+        Robot(name)
     }
 
     pub fn name(&self) -> &str {
