379dc0ba70870119453e8249cc24cde2b4dffbf1
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-03 18:23:42 -0500

    Implement encode

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,6 +1,20 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {plain:?} in Atbash cipher.");
+    plain
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(|c| {
+            if c.is_ascii_digit() {
+                c
+            } else {
+                (b'a' + b'z' - c.to_ascii_lowercase() as u8) as char
+            }
+        })
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
