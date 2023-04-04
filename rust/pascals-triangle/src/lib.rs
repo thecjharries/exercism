55c619601f9ec6436d34a6029d83ed6eabbee6aa
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-04 18:16:47 -0500

    Define some math tools

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,6 +1,22 @@
+use memoize::memoize;
+
 pub struct PascalsTriangle;
 
 impl PascalsTriangle {
+    #[memoize]
+    pub factorial(n: u32) -> u32 {
+        if n == 0 {
+            1
+        } else {
+            n * factorial(n - 1)
+        }
+    }
+
+    #[memoize]
+    pub fn binomial_coefficient(n: u32, k: u32) -> u32 {
+        factorial(n) / (factorial(k) * factorial(n - k))
+    }
+
     pub fn new(row_count: u32) -> Self {
         unimplemented!("create Pascal's triangle with {row_count} rows");
     }
