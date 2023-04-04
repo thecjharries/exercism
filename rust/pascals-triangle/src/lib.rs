e599f59df298a5f7fbb6aa44f29854ac08bd64ef
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-04 18:20:03 -0500

    Pull memoized fns out of PascalsTriangle

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -3,27 +3,27 @@ use memoize::memoize;
 #[derive(Debug, PartialEq)]
 pub struct PascalsTriangle(Vec<Vec<u32>>);
 
+#[memoize]
+pub fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+#[memoize]
+pub fn binomial_coefficient(n: u32, k: u32) -> u32 {
+    factorial(n) / (factorial(k) * factorial(n - k))
+}
+
 impl PascalsTriangle {
-    #[memoize]
-    pub factorial(n: u32) -> u32 {
-        if n == 0 {
-            1
-        } else {
-            n * factorial(n - 1)
-        }
-    }
-
-    #[memoize]
-    pub fn binomial_coefficient(n: u32, k: u32) -> u32 {
-        factorial(n) / (factorial(k) * factorial(n - k))
-    }
-
     pub fn row(n: u32) -> Vec<u32> {
         (0..=n).map(|k| binomial_coefficient(n, k)).collect()
     }
 
     pub fn new(row_count: u32) -> Self {
-        PascalsTriangle((0..row_count).map(|n| row(n)).collect())
+        PascalsTriangle((0..row_count).map(|n| Self::row(n)).collect())
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
