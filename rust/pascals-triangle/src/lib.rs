e7e3781260e1687f4ed9c51644f6112d08a0d83d
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-04 18:18:06 -0500

    Implement PascalsTriangle::new

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -23,7 +23,7 @@ impl PascalsTriangle {
     }
 
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {row_count} rows");
+        PascalsTriangle((0..row_count).map(|n| row(n)).collect())
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
