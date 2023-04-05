e2d470cb7145d782a5519d1413eb1b45277a8b06
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-05 17:55:17 -0500

    Create factorize fnc

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -5,6 +5,16 @@ pub enum Classification {
     Deficient,
 }
 
+pub fn factorize(num: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    for i in 1..num {
+        if num % i == 0 {
+            factors.push(i);
+        }
+    }
+    factors
+}
+
 pub fn classify(num: u64) -> Option<Classification> {
     unimplemented!("classify {num}");
 }
