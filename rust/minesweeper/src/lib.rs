f34de573a76a1dc986281035a4f8898d39f125f9
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-08 09:56:05 -0500

    Implement annotate

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,39 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let mut result = Vec::new();
+    for (row_index, row) in minefield.iter().enumerate() {
+        let mut annotated_row = String::new();
+        for (col_index, ch) in row.chars().enumerate() {
+            if ch == '*' {
+                annotated_row.push('*');
+            } else {
+                let mut count = 0;
+                for (i, j) in &[
+                    (row_index as i32 - 1, col_index as i32 - 1),
+                    (row_index as i32 - 1, col_index as i32),
+                    (row_index as i32 - 1, col_index as i32 + 1),
+                    (row_index as i32, col_index as i32 - 1),
+                    (row_index as i32, col_index as i32 + 1),
+                    (row_index as i32 + 1, col_index as i32 - 1),
+                    (row_index as i32 + 1, col_index as i32),
+                    (row_index as i32 + 1, col_index as i32 + 1),
+                ] {
+                    if *i >= 0
+                        && *i < minefield.len() as i32
+                        && *j >= 0
+                        && *j < row.len() as i32
+                        && minefield[*i as usize].chars().nth(*j as usize).unwrap() == '*'
+                    {
+                        count += 1;
+                    }
+                }
+                if count == 0 {
+                    annotated_row.push(' ');
+                } else {
+                    annotated_row.push_str(&count.to_string());
+                }
+            }
+        }
+        result.push(annotated_row);
+    }
+    result
 }
