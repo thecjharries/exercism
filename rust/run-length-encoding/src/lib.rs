96d4b7fbaca43ed89d83fdd75879355d4044a963
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-09 11:57:37 -0500

    Implement encode

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,5 +1,22 @@
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {source}.");
+    let mut index = 0;
+    let mut result = String::new();
+    while index < source.len() {
+        let mut count = 1;
+        let mut ch = source.chars().nth(index).unwrap();
+        while index + 1 < source.len() && source.chars().nth(index + 1).unwrap() == ch {
+            count += 1;
+            index += 1;
+        }
+        if count == 1 {
+            result.push(ch);
+        } else {
+            result.push_str(&count.to_string());
+            result.push(ch);
+        }
+        index += 1;
+    }
+    result
 }
 
 pub fn decode(source: &str) -> String {
