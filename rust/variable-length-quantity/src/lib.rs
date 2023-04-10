0baaf344dae3f277c5f283e63311c2342892b692
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-10 16:22:16 -0500

    Implement to_bytes

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -6,7 +6,25 @@ pub enum Error {
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!("Convert the values {values:?} to a list of bytes")
+    let mut result = Vec::new();
+    for value in values {
+        let mut binary = format!("{:b}", value);
+        if 0 != binary.len() % 7 {
+            binary = "0".repeat(7 - binary.len() % 7) + &binary;
+        }
+        for i in (0..binary.len() - 7).step_by(7) {
+            let mut byte = String::from("1");
+            byte.push_str(&binary[i..i + 7]);
+            if i + 7 >= binary.len() {
+                byte = format!("{:0>8}", byte);
+            }
+            result.push(u8::from_str_radix(&byte, 2).unwrap());
+        }
+        let mut byte = String::from("0");
+        byte.push_str(&binary[binary.len() - 7..binary.len()]);
+        result.push(u8::from_str_radix(&byte, 2).unwrap());
+    }
+    result
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
