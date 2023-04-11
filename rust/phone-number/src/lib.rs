ebad4d4e18394d39455df7a06275257b3a294788
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-11 18:26:28 -0500

    Implement number

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,5 +1,19 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{user_number}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None."
-    );
+    let mut number = user_number
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect::<String>();
+    if number.len() == 11 && number.starts_with('1') {
+        number.remove(0);
+    }
+    if 10 == number.len() {
+        let area_code = &number[0..3];
+        let exchange_code = &number[3..6];
+        if !area_code.starts_with('0') && !area_code.starts_with('1') {
+            if !exchange_code.starts_with('0') && !exchange_code.starts_with('1') {
+                return Some(number);
+            }
+        }
+    }
+    None
 }
