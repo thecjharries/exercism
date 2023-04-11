48a1c817abe9ea7d52fc6e681dfccc17176c416c
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-11 18:34:36 -0500

    Create RailFence::explode

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -1,7 +1,13 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 #[derive(Debug, PartialEq)]
 pub struct RailFence(u32);
 
 impl RailFence {
+    pub fn explode(text: &str) -> Vec<&str> {
+        test.graphemes(true).collect()
+    }
+
     pub fn new(rails: u32) -> RailFence {
         RailFence(rails)
     }
