142284f7cad1fc2684d62c35082245a1280a552b
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-11 18:40:50 -0500

    Implement RailFence::encode

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -5,7 +5,7 @@ pub struct RailFence(u32);
 
 impl RailFence {
     pub fn explode(text: &str) -> Vec<&str> {
-        test.graphemes(true).collect()
+        text.graphemes(true).collect()
     }
 
     pub fn new(rails: u32) -> RailFence {
@@ -13,7 +13,18 @@ impl RailFence {
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {text}")
+        let mut rails = vec![String::new(); self.0 as usize];
+        let exploded = RailFence::explode(text);
+        let mut rail: u32 = 0;
+        let mut direction: i32 = 1;
+        for c in exploded {
+            rails[rail as usize].push_str(c);
+            rail = (rail as i32 + direction) as u32;
+            if rail == 0 || rail == self.0 - 1 {
+                direction *= -1;
+            }
+        }
+        rails.join("")
     }
 
     pub fn decode(&self, cipher: &str) -> String {
