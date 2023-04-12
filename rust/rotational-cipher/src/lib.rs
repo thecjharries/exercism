1e11ba8a4bcdc76d7d0855471ff197d44185f26e
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-12 17:38:41 -0500

    Implement rotate

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,5 +1,18 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{input}' transform when every letter is shifted using key '{key}'?"
-    );
+    let key = key % 26;
+    let mut result = String::new();
+    for c in input.chars() {
+        if c.is_ascii_alphabetic() {
+            let mut new_c = c as u8;
+            if c.is_ascii_lowercase() {
+                new_c = (((new_c - b'a') as i8 + key) as u8) % 26 + b'a';
+            } else {
+                new_c = (((new_c - b'A') as i8 + key) as u8) % 26 + b'A';
+            }
+            result.push(new_c as char);
+        } else {
+            result.push(c);
+        }
+    }
+    result
 }
