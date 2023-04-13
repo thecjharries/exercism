415f065edd2d3471de13872c7a3f37e735bebf91
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-13 16:36:27 -0500

    Implement lsp

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -5,5 +5,22 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!("largest series product of a span of {span} digits in {string_digits}");
+    if 0 == span {
+        return Ok(1);
+    }
+    if string_digits.len() < span {
+        return Err(Error::SpanTooLong);
+    }
+    let digits = string_digits
+        .chars()
+        .map(|c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
+        .collect::<Result<Vec<_>, _>>()?;
+    let mut max = 0;
+    for window in digits.windows(span) {
+        let product = window.iter().product();
+        if product > max {
+            max = product;
+        }
+    }
+    Ok(max as u64)
 }
