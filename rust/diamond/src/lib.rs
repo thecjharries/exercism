48bac1514ee9081d31558f85ef9e4129082679a9
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-15 10:03:50 -0500

    Implement diamond

--- rust/diamond/src/lib.rs
+++ rust/diamond/src/lib.rs
@@ -1,5 +1,25 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    unimplemented!(
-        "Return the vector of strings which represent the diamond with particular char {c}"
-    );
+    if c == 'A' {
+        return vec!["A".to_string()];
+    }
+    let mut result = Vec::new();
+    let length = c as u8 - 'A' as u8 + 1;
+    for i in 0..length {
+        let current = 'A' as u8 + i;
+        let mut line = format!(
+            "{}{}{}",
+            " ".repeat((length - i - 1) as usize),
+            (current as char).to_string(),
+            " ".repeat((i) as usize)
+        );
+        let mut reverse = line.clone();
+        reverse.pop();
+        reverse = reverse.chars().rev().collect();
+        line.push_str(&reverse);
+        result.push(line);
+    }
+    for i in (0..length - 1).rev() {
+        result.push(result[i as usize].clone());
+    }
+    result
 }
