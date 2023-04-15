b172b4a35dde9f5becda4ddd3f21b6f574dfe8a8
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-15 10:19:41 -0500

    Define chunk fn format

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn encode(n: u64) -> String {
     unimplemented!("Say {n} in English.");
 }
+
+format(n: u64, divisor: u64, order: &str) -> String {
+    format!("{} {} {}", encode(n / divisor), order, encode(n % divisor))
+}
