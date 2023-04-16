53fdfb09943174f791273b49430a9d6ad77b6ed1
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-16 11:24:23 -0500

    Implement Luhn::is_valid

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -1,8 +1,25 @@
-pub struct Luhn(Vec<u8>);
+pub struct Luhn(Vec<char>);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        unimplemented!("Determine if the current Luhn struct contains a valid credit card number.");
+        if self.0.len() <= 1 {
+            return false;
+        }
+        let mut sum = 0;
+        for (i, &c) in self.0.iter().rev().enumerate() {
+            if !c.is_digit(10) {
+                return false;
+            }
+            let mut digit = c.to_digit(10).unwrap();
+            if i % 2 == 1 {
+                digit *= 2;
+                if digit > 9 {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+        }
+        sum % 10 == 0
     }
 }
 
