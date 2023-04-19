9d393412b121cc32cecc122197d12ab10eecafc5
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-18 19:20:41 -0500

    Implement decode

--- rust/simple-cipher/src/lib.rs
+++ rust/simple-cipher/src/lib.rs
@@ -20,7 +20,24 @@ pub fn encode(key: &str, s: &str) -> Option<String> {
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
-    unimplemented!("Use {key} to decode {s} using shift cipher")
+    if 0 == key.chars().filter(|c| c.is_ascii_lowercase()).count() {
+        return None;
+    }
+    let mut result = String::new();
+    let mut key_iter = key.chars().cycle();
+    for c in s.chars() {
+        if c.is_ascii_alphabetic() {
+            let key_char = key_iter.next().unwrap();
+            if key_char.is_ascii_lowercase() {
+                result.push(((c as u8 + 26 - key_char as u8 + b'a' - b'a') % 26 + b'a') as char)
+            } else {
+                return None;
+            }
+        } else {
+            result.push(c);
+        }
+    }
+    Some(result)
 }
 
 pub fn encode_random(s: &str) -> (String, String) {
