58105c78840da5dec48d1c5eeda85c0b2a1d863a
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-20 16:56:00 -0500

    Define STUDENTS global const

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,3 +1,8 @@
+const STUDENTS: [&str; 12] = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+    "Kincaid", "Larry",
+];
+
 pub fn plants(_diagram: &str, _student: &str) -> Vec<&'static str> {
     unimplemented!("Solve kindergarten-garden exercise");
 }
