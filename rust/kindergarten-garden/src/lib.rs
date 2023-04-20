cc383d762f8966f2694d970908d9ac84dbadef65
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-20 17:00:19 -0500

    Implement plants

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -4,5 +4,24 @@ const STUDENTS: [&str; 12] = [
 ];
 
 pub fn plants(_diagram: &str, _student: &str) -> Vec<&'static str> {
-    unimplemented!("Solve kindergarten-garden exercise");
+    let index = STUDENTS.iter().position(|&s| s == _student).unwrap();
+    let mut plants: Vec<Vec<char>> = _diagram
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>();
+    println!("{:?}", plants);
+    let mut result = Vec::new();
+    for i in 0..2 {
+        for j in 0..2 {
+            let plant = plants[i][2 * index + j];
+            result.push(match plant {
+                'R' => "radishes",
+                'C' => "clover",
+                'G' => "grass",
+                'V' => "violets",
+                _ => unreachable!(),
+            });
+        }
+    }
+    result
 }
