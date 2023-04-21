11ac332d8814f5ee9713c354e269a636f9ab9b90
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-21 17:48:51 -0500

    Create gcd fnc

--- rust/affine-cipher/src/lib.rs
+++ rust/affine-cipher/src/lib.rs
@@ -5,6 +5,14 @@ pub enum AffineCipherError {
     NotCoprime(i32),
 }
 
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
 /// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
 /// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
