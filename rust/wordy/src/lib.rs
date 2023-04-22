3ffc7398f8bb4d555db6bcdbc1f80023df6caccb
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-22 10:34:40 -0500

    Define basic number syntax

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,5 +1,11 @@
 pub fn answer(command: &str) -> Option<i32> {
-    unimplemented!(
-        "Return the result of the command '{command}' or None, if the command is invalid."
-    );
+    let command = command
+        .trim_start_matches("What is ")
+        .trim_end_matches('?')
+        .split_whitespace()
+        .collect::<Vec<&str>>();
+    if 1 == command.len() {
+        return command[0].parse::<i32>().ok();
+    }
+    unimplemented!("Return the result of the command '' or None, if the command is invalid.");
 }
