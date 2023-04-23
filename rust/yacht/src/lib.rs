099eedd11b7d9e489cb13efa1f3631040b01da01
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-23 15:24:03 -0500

    Implement Full House

--- rust/yacht/src/lib.rs
+++ rust/yacht/src/lib.rs
@@ -23,6 +23,19 @@ pub fn score(_dice: Dice, _category: Category) -> u8 {
         Category::Fours => _dice.iter().filter(|&x| *x == 4).sum(),
         Category::Fives => _dice.iter().filter(|&x| *x == 5).sum(),
         Category::Sixes => _dice.iter().filter(|&x| *x == 6).sum(),
+        Category::FullHouse => {
+            let mut counts = [0; 6];
+            for &d in _dice.iter() {
+                counts[d as usize - 1] += 1;
+            }
+            if counts.iter().filter(|&x| *x == 2).count() == 1
+                && counts.iter().filter(|&x| *x == 3).count() == 1
+            {
+                _dice.iter().sum()
+            } else {
+                0
+            }
+        }
         _ => 0,
     }
 }
