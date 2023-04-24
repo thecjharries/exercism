313bc5691acdbe8d957f5903fad02e36368bf9ab
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-24 17:54:06 -0500

    Implment actions

--- rust/secret-handshake/src/lib.rs
+++ rust/secret-handshake/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    unimplemented!("What is the secret handshake for {n}?")
+    let actions = format!("{:05b}", n).chars().rev().collect::<Vec<_>>();
+    let mut result = vec![];
+    if '1' == actions[0] {
+        result.push("wink");
+    }
+    if '1' == actions[1] {
+        result.push("double blink");
+    }
+    if '1' == actions[2] {
+        result.push("close your eyes");
+    }
+    if '1' == actions[3] {
+        result.push("jump");
+    }
+    if '1' == actions[4] {
+        result.reverse();
+    }
+    result
 }
