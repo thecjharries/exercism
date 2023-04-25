3eef526b4edf52e8d3b1dd7f0bda1fa6600a1d76
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-25 16:31:18 -0500

    Implement naive solution using power set

--- rust/knapsack/src/lib.rs
+++ rust/knapsack/src/lib.rs
@@ -1,8 +1,25 @@
+use itertools::Itertools;
+
 pub struct Item {
     pub weight: u32,
     pub value: u32,
 }
 
 pub fn maximum_value(_max_weight: u32, _items: &[Item]) -> u32 {
-    unimplemented!("Solve the knapsack exercise");
+    let items = _items
+        .iter()
+        .filter(|item| item.weight <= _max_weight)
+        .collect_vec();
+    let powerset = items.iter().powerset();
+    let mut max_value = 0;
+    for subset in powerset {
+        let total_weight: u32 = subset.iter().map(|item| item.weight).sum();
+        if total_weight <= _max_weight {
+            let total_value: u32 = subset.iter().map(|item| item.value).sum();
+            if total_value > max_value {
+                max_value = total_value;
+            }
+        }
+    }
+    max_value
 }
