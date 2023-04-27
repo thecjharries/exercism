b8d15ad5c3a10e25a32c91eb7cd3f238c5819ec0
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-27 17:54:13 -0500

    Create fnc to score single letter

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -1,3 +1,16 @@
+fn score_letter(letter: char) -> u64 {
+    match letter {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 0,
+    }
+}
+
 /// Compute the Scrabble score for a word.
 pub fn score(word: &str) -> u64 {
     unimplemented!("Score {word} in Scrabble.");
