28191aac054926c70e7ca0e9b443864c4796c67a
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-30 09:54:45 -0500

    Implement Scale::new

--- rust/scale-generator/src/lib.rs
+++ rust/scale-generator/src/lib.rs
@@ -16,7 +16,28 @@ pub struct Scale(Vec<String>);
 
 impl Scale {
     pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        unimplemented!("Construct a new scale with tonic {tonic} and intervals {intervals}")
+        let mut scale = Vec::new();
+        let notes = match tonic {
+            "C" | "a" | "G" | "D" | "A" | "E" | "B" | "F#" | "e" | "b" | "f#" | "c#" | "g#"
+            | "d#" => SHARPS,
+            "F" | "Bb" | "Eb" | "Ab" | "Db" | "Gb" | "d" | "g" | "c" | "f" | "bb" | "eb" => FLATS,
+            _ => return Err(Error::InvalidTonic),
+        };
+        let mut index = notes
+            .iter()
+            .position(|&x| x.to_uppercase() == tonic.to_uppercase())
+            .unwrap();
+        scale.push(notes[index].to_string());
+        for interval in intervals.chars() {
+            index += match interval {
+                'm' => 1,
+                'M' => 2,
+                'A' => 3,
+                _ => return Err(Error::InvalidInterval),
+            };
+            scale.push(notes[index % 12].to_string());
+        }
+        Ok(Scale(scale))
     }
 
     pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
