eabedfd8579d686010ac946e56fbdcd3bb4808cc
CJ Harries <cj@wizardsoftheweb.pro>
2023-04-30 09:42:00 -0500

    Define const and error

--- rust/scale-generator/src/lib.rs
+++ rust/scale-generator/src/lib.rs
@@ -1,16 +1,15 @@
-// You should change this.
-//
-// Depending on your implementation, there are a variety of potential errors
-// which might occur. They aren't checked by the test suite in order to
-// allow the greatest freedom of implementation, but real libraries should
-// provide useful, descriptive errors so that downstream code can react
-// appropriately.
-//
-// One common idiom is to define an Error enum which wraps all potential
-// errors. Another common idiom is to use a helper type such as failure::Error
-// which does more or less the same thing but automatically.
+const SHARPS: [&str; 12] = [
+    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+];
+const FLATS: [&str; 12] = [
+    "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
+];
+
 #[derive(Debug)]
-pub struct Error;
+pub enum Error {
+    InvalidTonic,
+    InvalidInterval,
+}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Scale(Vec<String>);
