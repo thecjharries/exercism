55eec23b64f105ca5e4ce898ae08fdf4b1d4f355
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-02 16:26:17 -0500

    Refactor with messages

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,29 +1,37 @@
+use std::cmp::min;
 use std::collections::HashMap;
-use std::rc::Rc;
-use std::sync::Mutex;
+use std::sync::mpsc;
+use std::sync::Arc;
 use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let frequencies: Rc<Mutex<HashMap<char, usize>>> = Rc::new(Mutex::new(HashMap::new()));
-    let mut handles = Vec::new();
-    for text in input.iter() {
-        let frequencies = Rc::clone(&frequencies);
-        let handle = thread::spawn(move || {
-            let mut map = frequencies.lock().unwrap();
-            for c in text.to_lowercase().chars().filter(|c| c.is_alphabetic()) {
-                let counter = map.entry(c).or_insert(0);
-                *counter += 1;
+    let input = input.join("");
+    let mut data = input.chars();
+    let worker_count = min(worker_count, input.len());
+    let mut chunk_size = (input.len() / worker_count).max(1);
+    if input.len() > chunk_size * worker_count {
+        chunk_size += 1;
+    }
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..worker_count {
+        let tx = tx.clone();
+        let chunk = data.by_ref().take(chunk_size).collect::<Vec<char>>();
+        thread::spawn(move || {
+            let mut map = HashMap::new();
+            for c in chunk {
+                if c.is_alphabetic() {
+                    *map.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
+                }
             }
+            tx.send(map).unwrap();
         });
-        handles.push(handle);
     }
-    for handle in handles {
-        handle.join().unwrap();
+    drop(tx);
+    let mut map = HashMap::new();
+    for m in rx {
+        for (k, v) in m {
+            *map.entry(k).or_insert(0) += v;
+        }
     }
-    let mut map = frequencies.lock().unwrap();
-    let mut result = HashMap::new();
-    for (k, v) in map.drain() {
-        result.insert(k, v);
-    }
-    result
+    map
 }
