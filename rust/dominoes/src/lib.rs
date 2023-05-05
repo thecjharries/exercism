4cac7944353c1b7d877d3dab99b1d559a5cf1cef
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-05 16:02:21 -0500

    Draft chaining algo

--- rust/dominoes/src/lib.rs
+++ rust/dominoes/src/lib.rs
@@ -1,5 +1,24 @@
 use itertools::Itertools;
 
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
+    for mut possible_chain in input.into_iter().permutations(input.len()).unique() {
+        let result: Vec<(u8, u8)> = possible_chain.iter().map(|&x| *x).collect();
+        let first_element = possible_chain.first().copied().unwrap();
+        let last_element = possible_chain.last().copied().unwrap();
+        if first_element.0 != last_element.1 {
+            continue;
+        }
+        let mut current_element = possible_chain.pop().unwrap();
+        while let Some(next_element) = possible_chain.pop() {
+            // println!("{:?} - {:?}", current_element, next_element);
+            if current_element.0 != next_element.1 {
+                break;
+            }
+            current_element = next_element;
+        }
+        if possible_chain.is_empty() {
+            return Some(result);
+        }
+    }
     None
 }
