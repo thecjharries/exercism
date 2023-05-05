5c7555e3245bbfebbae0477ebe1e576e1246f3d7
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-05 15:47:15 -0500

    Implement base case

--- rust/dominoes/src/lib.rs
+++ rust/dominoes/src/lib.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
-    unimplemented!("From the given input '{input:?}' construct a proper dominoes chain or return None if it is not possible.");
+    None
 }
