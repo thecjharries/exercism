b29a7aa030583b1cf6496098978a55c44e22ae21
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-08 18:08:24 -0500

    Imlement BowlingGame::roll

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -5,7 +5,7 @@ pub enum Error {
 }
 
 struct Frame {
-    first: u16,
+    first: Option<u16>,
     second: Option<u16>,
     third: Option<u16>,
 }
@@ -24,7 +24,54 @@ impl BowlingGame {
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {pins} pins have been scored");
+        match self.current_frame {
+            10..=usize::MAX => Err(Error::GameComplete),
+            9 => {
+                let frame = self.frames.get_mut(self.current_frame).unwrap();
+                if frame.first.is_none() {
+                    frame.first = Some(pins);
+
+                    Ok(())
+                } else if 10 == frame.first.unwrap() {
+                    if 10 == frame.second.unwrap() {
+                        frame.third = Some(pins);
+                        self.current_frame += 1;
+                        Ok(())
+                    } else if frame.second.is_none() {
+                        frame.second = Some(pins);
+                        Ok(())
+                    } else if frame.second.unwrap() + pins > 10 {
+                        Err(Error::NotEnoughPinsLeft)
+                    } else {
+                        frame.third = Some(pins);
+                        self.current_frame += 1;
+                        Ok(())
+                    }
+                } else if frame.first.unwrap() + pins > 10 {
+                    Err(Error::NotEnoughPinsLeft)
+                } else {
+                    frame.second = Some(pins);
+                    self.current_frame += 1;
+                    Ok(())
+                }
+            }
+            _ => {
+                let frame = self.frames.get_mut(self.current_frame).unwrap();
+                if frame.first.is_none() {
+                    frame.first = Some(pins);
+                    if 10 == pins {
+                        self.current_frame += 1;
+                    }
+                    Ok(())
+                } else if frame.first.unwrap() + pins > 10 {
+                    Err(Error::NotEnoughPinsLeft)
+                } else {
+                    frame.second = Some(pins);
+                    self.current_frame += 1;
+                    Ok(())
+                }
+            }
+        }
     }
 
     pub fn score(&self) -> Option<u16> {
