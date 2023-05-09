16fcc52588b895a51aa3cd2f0d709e984ae69b39
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-09 15:56:24 -0500

    Implement hashmap macro

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,6 +1,17 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        unimplemented!()
+    ()  => {
+        {
+            ::std::collections::HashMap::new()
+        }
+    };
+    ( $($key:expr => $value:expr),+  $(,)? ) => {
+        {
+            let mut map = ::std::collections::HashMap::new();
+            $(
+                map.insert($key, $value);
+            )*
+            map
+        }
     };
 }
