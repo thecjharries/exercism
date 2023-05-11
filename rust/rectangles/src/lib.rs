47db1042aaf9f458b3c9ae1efba70c29a5a5a27a
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-11 17:18:15 -0500

    Implement count

--- rust/rectangles/src/lib.rs
+++ rust/rectangles/src/lib.rs
@@ -1,3 +1,31 @@
 pub fn count(lines: &[&str]) -> u32 {
-    unimplemented!("\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{lines:#?}\n.");
+    let grid: Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
+    let mut rectangle_count = 0;
+    for (y, row) in grid.iter().enumerate() {
+        for (x, character) in row.iter().enumerate() {
+            if '+' != *character {
+                continue;
+            }
+            let mut x_corners = Vec::new();
+            for next_x in x + 1..row.len() {
+                if '+' == row[next_x] {
+                    x_corners.push(next_x);
+                }
+            }
+            let mut y_corners = Vec::new();
+            for next_y in y + 1..grid.len() {
+                if '+' == grid[next_y][x] {
+                    y_corners.push(next_y);
+                }
+            }
+            for &next_x in &x_corners {
+                for &next_y in &y_corners {
+                    if '+' == grid[next_y][next_x] {
+                        rectangle_count += 1;
+                    }
+                }
+            }
+        }
+    }
+    rectangle_count
 }
