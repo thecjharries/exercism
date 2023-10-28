84ae07b303264033873a7d80a5ccd187f6b63bc9
CJ Harries <cj@wizardsoftheweb.pro>
2023-10-28 12:07:54 -0500

    Implement from

--- common-lisp/gigasecond-anniversary/gigasecond-anniversary.lisp
+++ common-lisp/gigasecond-anniversary/gigasecond-anniversary.lisp
@@ -3,4 +3,12 @@
   (:export :from))
 (in-package :gigasecond-anniversary)
 
-(defun from (year month day hour minute second))
+(defun from (year month day hour minute second) (
+  multiple-value-bind (second minute hour day month year)
+    (decode-universal-time
+     (+
+      (encode-universal-time second minute hour day month year 0)
+      (expt 10 9)) 0)
+    (list year month day hour minute second)
+))
+
