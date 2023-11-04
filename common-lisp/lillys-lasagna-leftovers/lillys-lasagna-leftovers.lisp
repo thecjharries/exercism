cd53a1407bc63081cab2f729ca22d2fab6c47a2d
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-04 13:00:26 -0500

    Implement preparation-time

--- common-lisp/lillys-lasagna-leftovers/lillys-lasagna-leftovers.lisp
+++ common-lisp/lillys-lasagna-leftovers/lillys-lasagna-leftovers.lisp
@@ -8,6 +8,9 @@
 (in-package :lillys-lasagna-leftovers)
 
 ;; Define function preparation-time
+(defun preparation-time (&rest layers) (
+  * 19 (length layers)
+))
 
 ;; Define function remaining-minutes-in-oven
 
