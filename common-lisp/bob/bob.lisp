f6bf7b433c2fccfd271ea302371f38831a2d5f48
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-09 08:31:01 -0600

    Implement remaining cases

--- common-lisp/bob/bob.lisp
+++ common-lisp/bob/bob.lisp
@@ -5,5 +5,8 @@
 
 (defun response (hey-bob)
   (setf hey-bob (remove #\Space (remove #\Newline (remove #\Tab hey-bob))))
-  (cond ((zerop (length hey-bob)) "Fine. Be that way")
+  (cond ((zerop (length hey-bob)) "Fine. Be that way!")
+    ((and (and (string= hey-bob (string-upcase hey-bob)) (some #'alpha-char-p hey-bob)) (char= #\? (uiop:last-char hey-bob))) "Calm down, I know what I'm doing!")
+    ((and (string= hey-bob (string-upcase hey-bob)) (some #'alpha-char-p hey-bob)) "Whoa, chill out!")
+    ((char= #\? (uiop:last-char hey-bob)) "Sure.")
     (t "Whatever.")))
