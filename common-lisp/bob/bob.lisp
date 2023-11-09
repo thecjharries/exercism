4258add7946498d2e4c36f3b0743cc6ba032ff00
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-09 08:26:20 -0600

    Implement base case and zero length

--- common-lisp/bob/bob.lisp
+++ common-lisp/bob/bob.lisp
@@ -3,4 +3,7 @@
   (:export :response))
 (in-package :bob)
 
-(defun response (hey-bob))
+(defun response (hey-bob)
+  (setf hey-bob (remove #\Space (remove #\Newline (remove #\Tab hey-bob))))
+  (cond ((zerop (length hey-bob)) "Fine. Be that way")
+    (t "Whatever.")))
