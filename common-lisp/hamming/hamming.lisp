dda93ef4ce1babbe75ebed19f9dd611a11af6662
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-10 08:01:01 -0600

    Implement hamming distance

--- common-lisp/hamming/hamming.lisp
+++ common-lisp/hamming/hamming.lisp
@@ -6,4 +6,8 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
+  (cond ((not (equal (length dna1) (length dna2))) nil)
+        ((equal dna1 dna2) 0)
+        (t (loop for i from 0 below (length dna1)
+                 count (not (equal (char dna1 i) (char dna2 i))))))
   )
