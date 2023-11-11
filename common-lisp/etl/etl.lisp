12e7db157fc1385823835effc9760b2ff1f3d4c3
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-11 13:24:46 -0600

    Implement transform

--- common-lisp/etl/etl.lisp
+++ common-lisp/etl/etl.lisp
@@ -6,4 +6,11 @@
 
 (defun transform (data)
   "Transforms hash values into keys with their keys as their values."
-  )
+  (let ((result (make-hash-table)))
+    (maphash #'(lambda (key value)
+               (mapcar #'(lambda (letter)
+                         (setf (gethash (char-downcase letter) result) key))
+                       value))
+             data)
+    result
+  ))
