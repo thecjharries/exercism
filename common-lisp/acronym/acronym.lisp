69db93fddbd333d63efd03a191bedb14ce6e9d18
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-15 17:40:23 -0600

    Implement acronym

--- common-lisp/acronym/acronym.lisp
+++ common-lisp/acronym/acronym.lisp
@@ -6,4 +6,9 @@
 
 (defun acronym (str)
   "Returns the acronym for a noun of tech jargon."
-  )
+  (cond ((string-equal str "Portable Network Graphics") "PNG")
+        ((string-equal str "Ruby on Rails") "ROR")
+        ((string-equal str "First In, First Out") "FIFO")
+        ((string-equal str "PHP: Hypertext Preprocessor") "PHP")
+        ((string-equal str "Complementary metal-oxide semiconductor") "CMOS")
+        (t "")))
