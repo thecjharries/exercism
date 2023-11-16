8bd2cc7c83d885b0e7785bf59897aa6d633f753b
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-16 12:28:36 -0600

    Implement armstrong-number-p

--- common-lisp/armstrong-numbers/armstrong-numbers.lisp
+++ common-lisp/armstrong-numbers/armstrong-numbers.lisp
@@ -3,4 +3,6 @@
   (:export :armstrong-number-p))
 (in-package :armstrong-numbers)
 
-(defun armstrong-number-p (number))
+(defun armstrong-number-p (number)
+  (let ((digits (map 'list #'digit-char-p (prin1-to-string number))))
+    (= number (reduce #'+ (mapcar (lambda (digit) (expt digit (length digits))) digits)))))
