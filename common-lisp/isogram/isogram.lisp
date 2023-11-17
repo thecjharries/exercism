5bdca5a97c8b2bf45d4e87341294c97e6b77b73b
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-17 07:21:59 -0600

    Implement isogram-p

--- common-lisp/isogram/isogram.lisp
+++ common-lisp/isogram/isogram.lisp
@@ -5,4 +5,6 @@
 (in-package :isogram)
 
 (defun isogram-p (string)
-  "Is string an Isogram?")
+  "Is string an Isogram?"
+  (let ((comparison (remove-if-not #'alpha-char-p (string-downcase string))))
+    (string= comparison (remove-duplicates comparison))))
