694a634fa14a28ad0dbed987f0c2c96c271ac5f5
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-18 11:18:52 -0600

    Implement binary-find

--- common-lisp/binary-search/binary-search.lisp
+++ common-lisp/binary-search/binary-search.lisp
@@ -5,4 +5,12 @@
 (in-package :binary-search)
 
 (defun binary-find (arr el)
-  )
+  (let ((low 0)
+        (high (1- (length arr))))
+    (loop while (<= low high)
+          do (let ((mid (floor (+ low high) 2)))
+               (cond ((= (aref arr mid) el) (return-from binary-find mid))
+                     ((< (aref arr mid) el) (setf low (1+ mid)))
+                     (t (setf high (1- mid))))))
+    nil))
+
