dddaf578ae7a02ee192496b911b2f22ebac1841a
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-19 18:11:14 -0600

    Implememnt collatz

--- common-lisp/collatz-conjecture/collatz-conjecture.lisp
+++ common-lisp/collatz-conjecture/collatz-conjecture.lisp
@@ -4,4 +4,8 @@
 
 (in-package :collatz-conjecture)
 
-(defun collatz (n))
+(defun collatz (n &optional (steps 0)) (
+  cond ((< n 1) nil)
+       ((= n 1) steps)
+       ((evenp n) (collatz (/ n 2) (1+ steps)))
+       (t (collatz (+ (* n 3) 1) (1+ steps)))))
