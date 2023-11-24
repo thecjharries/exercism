62d25bcb7702a5db16d2bbcd73bc8d2f46d884c7
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-24 09:29:57 -0600

    Implement convert

--- common-lisp/raindrops/raindrops.lisp
+++ common-lisp/raindrops/raindrops.lisp
@@ -6,4 +6,9 @@
 
 (defun convert (n)
   "Converts a number to a string of raindrop sounds."
+  (let ((result (concatenate 'string
+                             (if (zerop (mod n 3)) "Pling")
+                             (if (zerop (mod n 5)) "Plang")
+                             (if (zerop (mod n 7)) "Plong"))))
+    (if (string= result "") (write-to-string n) result))
   )
