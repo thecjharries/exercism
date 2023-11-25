da4dddc15a662287c123c8df82c2cdd50966326f
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-25 11:19:58 -0600

    Implement primes-to

--- common-lisp/sieve/sieve.lisp
+++ common-lisp/sieve/sieve.lisp
@@ -7,4 +7,11 @@
 
 (defun primes-to (n)
   "List primes up to `n' using sieve of Eratosthenes."
-  )
+  (let ((primes (loop for i from 2 to n collect i))
+        (p 2))
+    (loop while (<= (* p p) n)
+          do (setf primes (remove-if (lambda (x) (and (not (eq x p))
+                                                      (zerop (mod x p))))
+                                     primes))
+          do (setf p (find-if (lambda (x) (> x p)) primes)))
+    primes))
