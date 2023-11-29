471b363b27251d14d279f6056f1febb540d3ec1c
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-28 18:34:44 -0600

    Implement triangle-type-p

--- common-lisp/triangle/triangle.lisp
+++ common-lisp/triangle/triangle.lisp
@@ -5,10 +5,29 @@
 (in-package :triangle)
 
 (defun is-triangle-p (a b c)
-  (and (<= (+ a b) c)
-       (<= (+ a c) b)
-       (<= (+ b c) a)))
+  (and (< 0 a)
+       (< 0 b)
+       (< 0 c)
+       (>= (+ a b) c)
+       (>= (+ a c) b)
+       (>= (+ b c) a)))
+
+(defun is-isosceles-p (a b c)
+  (or (= a b)
+      (= b c)
+      (= a c)))
+
+(defun is-equilateral-p (a b c)
+  (= a b c))
+
+(defun is-scalene-p (a b c)
+  (not (is-isosceles-p a b c)))
 
 (defun triangle-type-p (type a b c)
   "Deterimines if a triangle (given by side lengths A, B, C) is of the given TYPE"
-  )
+  (and (is-triangle-p a b c)
+       (case type
+         (:equilateral (is-equilateral-p a b c))
+         (:isosceles (is-isosceles-p a b c))
+         (:scalene (is-scalene-p a b c))
+         (otherwise nil))))
