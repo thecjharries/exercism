ba8a6bc0b32e39f655eb25e9651429e502d15226
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-29 17:52:55 -0600

    Implement anagrams-for

--- common-lisp/anagram/anagram.lisp
+++ common-lisp/anagram/anagram.lisp
@@ -4,8 +4,13 @@
 
 (in-package :anagram)
 
-(defun convert-to-sorted-char-list (word) (sort (every #'alpha-char-p (lower-case-p word)))
+(defun convert-to-sorted-char-list (word) (sort (coerce (string-upcase word) 'list) #'char-lessp))
 
 (defun anagrams-for (subject candidates)
   "Returns a sublist of candidates which are anagrams of the subject."
-  )
+  (let ((possible (copy-list candidates)))
+    (remove-if-not (lambda (candidate)
+                     (and (not (equal (string-upcase candidate) (string-upcase subject)))
+                          (equal (convert-to-sorted-char-list candidate)
+                                 (convert-to-sorted-char-list subject))))
+                   possible)))
