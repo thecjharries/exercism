39f80c0fa67bf29352750c5f5cac68614a65a455
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-28 18:12:35 -0600

    Implement triangle inequality fnc

--- common-lisp/triangle/triangle.lisp
+++ common-lisp/triangle/triangle.lisp
@@ -4,6 +4,11 @@
 
 (in-package :triangle)
 
+(defun is-triangle-p (a b c)
+  (and (<= (+ a b) c)
+       (<= (+ a c) b)
+       (<= (+ b c) a)))
+
 (defun triangle-type-p (type a b c)
   "Deterimines if a triangle (given by side lengths A, B, C) is of the given TYPE"
   )
