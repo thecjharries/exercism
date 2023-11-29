cf35f5c8a13f13a2edf7484b8f599376532a512e
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-29 17:40:18 -0600

    Create common-lisp:anagram exercise

---
+++ common-lisp/anagram/anagram.lisp
@@ -0,0 +1,9 @@
+(defpackage :anagram
+  (:use :cl)
+  (:export :anagrams-for))
+
+(in-package :anagram)
+
+(defun anagrams-for (subject candidates)
+  "Returns a sublist of candidates which are anagrams of the subject."
+  )
