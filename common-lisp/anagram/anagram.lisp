d467a790e9c5611387c48b54b02e038f678534e7
CJ Harries <cj@wizardsoftheweb.pro>
2023-11-29 17:40:18 -0600

    Add Makefile

--- common-lisp/anagram/anagram.lisp
+++ common-lisp/anagram/anagram.lisp
@@ -4,6 +4,8 @@
 
 (in-package :anagram)
 
+(defun convert-to-sorted-char-list (word) (sort (every #'alpha-char-p (lower-case-p word)))
+
 (defun anagrams-for (subject candidates)
   "Returns a sublist of candidates which are anagrams of the subject."
   )
