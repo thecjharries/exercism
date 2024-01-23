f41c90007e599a28506139de0c2df35e2d65e5d8
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-23 17:29:00 -0600

    Solve exercise

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,14 @@
 #include "reverse_string.h"
+#include <algorithm>
+#include <string>
 
-namespace reverse_string {
+namespace reverse_string
+{
 
-}  // namespace reverse_string
+    std::string reverse_string(std::string input)
+    {
+        std::reverse(input.begin(), input.end());
+        return input;
+    }
+
+} // namespace reverse_string
