46bddbba04bc07859d299d3c2962cce8c5e7fdcd
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-27 17:49:46 -0600

    Add global exercise creation

--- global.mk
+++ global.mk
@@ -5,3 +5,8 @@ debug::
 	@echo "IN: $(IN)"
 	@echo "SUBMISSIONS: $(SUBMISSIONS)"
 	@echo "TRACK: $(TRACK)"
+
+# Create the exercise
+.PHONY: create
+create:
+	$(EXERCISM) download --exercise=$(EXERCISE) --track=$(TRACK)
