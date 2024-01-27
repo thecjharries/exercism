96ce08005ddea81645451f4fc4fb0bbefbe75ec3
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-27 17:51:31 -0600

    Create global makefile creation

--- global.mk
+++ global.mk
@@ -10,3 +10,18 @@ debug::
 .PHONY: create
 create:
 	$(EXERCISM) download --exercise=$(EXERCISE) --track=$(TRACK)
+
+# Create the feature branch
+.PHONY: boot-feature-branch
+boot-feature-branch: create
+	$(GIT) checkout -b feat/$(TRACK)/$(EXERCISE)
+	$(GIT) add $(EXERCISE)
+	$(GIT) commit -m "Create $(TRACK):$(EXERCISE) exercise"
+
+# Create exercise Makefile
+.PHONY: boot-makefile
+boot-makefile: boot-feature-branch
+	echo "-include ../Makefile" >> $(EXERCISE)/Makefile
+	echo "-include ../../.global.mk" >> $(EXERCISE)/Makefile
+	$(GIT) add $(EXERCISE)/Makefile
+	$(GIT) commit -m "Add Makefile"
