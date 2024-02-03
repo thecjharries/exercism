ed10ed57303eccc4f47b1eabd5064867c45a0309
CJ Harries <cj@wizardsoftheweb.pro>
2024-02-02 18:17:27 -0600

    Solve the exercise

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,7 +1,10 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
+class Raindrops
+    def self.convert(number)
+        result = ''
+        result += 'Pling' if number % 3 == 0
+        result += 'Plang' if number % 5 == 0
+        result += 'Plong' if number % 7 == 0
+        result = number.to_s if result.empty?
+        result
+    end
+end
