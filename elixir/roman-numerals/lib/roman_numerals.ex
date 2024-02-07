9939bbe386f6858207df726018df8c6276b8de08
CJ Harries <cj@wizardsoftheweb.pro>
2024-02-07 17:32:40 -0600

    Generate solution with Copilot

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -4,5 +4,33 @@ defmodule RomanNumerals do
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
+    if number < 1 or number > 3000 do
+      raise ArgumentError, "Number must be between 1 and 3000"
+    end
+
+    roman_numerals = [
+      {1000, "M"},
+      {900, "CM"},
+      {500, "D"},
+      {400, "CD"},
+      {100, "C"},
+      {90, "XC"},
+      {50, "L"},
+      {40, "XL"},
+      {10, "X"},
+      {9, "IX"},
+      {5, "V"},
+      {4, "IV"},
+      {1, "I"}
+    ]
+
+    div_rem = fn a, b -> {div(a, b), rem(a, b)} end
+
+    roman_numerals
+    |> Enum.reduce({number, ""}, fn {arabic, roman}, {n, acc} ->
+      {div, rem} = div_rem.(n, arabic)
+      {rem, acc <> String.duplicate(roman, div)}
+    end)
+    |> elem(1)
   end
 end
