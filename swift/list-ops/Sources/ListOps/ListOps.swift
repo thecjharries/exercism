12c52405c75690eecdaebe82351c2d4fa5a720a6
CJ Harries <cj@wizardsoftheweb.pro>
2024-02-23 16:13:11 -0600

    Add concat

--- swift/list-ops/Sources/ListOps/ListOps.swift
+++ swift/list-ops/Sources/ListOps/ListOps.swift
@@ -2,4 +2,8 @@ struct ListOps {
     static func append<T>(_ list1: [T], _ list2: [T]) -> [T] {
         return list1 + list2
     }
+
+    static func concat<T>(_ args: [T]...) -> [T] {
+        return args.reduce([], +)
+    }
 }
