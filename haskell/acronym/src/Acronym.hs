b3b95d91fe1d944352382b166862df236fdadfc9
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-11 17:46:27 -0500

    Solve exercise

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,11 @@
 module Acronym (abbreviate) where
 
+import Data.Char (isAlpha, isLower, isUpper, toUpper)
+
+m :: (Char, Char) -> String
+m (x, y) | not (isAlpha x || x == '\'') && isAlpha y = [toUpper y]
+m (x, y) | isLower x && isUpper y = [toUpper y]
+m _ = []
+
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate s = concatMap m $ zip (" " <> s) s
