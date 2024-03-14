821b08ca4665e78b04813150f1fb840a23f5874c
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-14 15:46:25 -0500

    Define type

--- c/circular-buffer/circular_buffer.h
+++ c/circular-buffer/circular_buffer.h
@@ -1,4 +1,14 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
 
+typedef int buffer_value_t;
+
+typedef struct
+{
+    buffer_value_t *buffer;
+    int capacity;
+    int head;
+    int tail;
+} circular_buffer_t;
+
 #endif
