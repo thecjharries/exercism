a807da59761bfe502b0d9e10aad7684db15b7234
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-14 16:04:16 -0500

    Solve exercise

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,1 +1,69 @@
 #include "circular_buffer.h"
+
+#include <stdlib.h>
+#include <errno.h>
+
+circular_buffer_t *new_circular_buffer(int capacity)
+{
+    circular_buffer_t *buffer = malloc(sizeof(circular_buffer_t));
+    buffer->buffer = malloc(capacity * sizeof(buffer_value_t));
+    buffer->capacity = capacity;
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->full = 0;
+    return buffer;
+}
+
+void clear_buffer(circular_buffer_t *buffer)
+{
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->full = 0;
+}
+
+void delete_buffer(circular_buffer_t *buffer)
+{
+    free(buffer->buffer);
+    free(buffer);
+}
+
+unsigned int write(circular_buffer_t *buffer, buffer_value_t value)
+{
+    if (buffer->full)
+    {
+        errno = ENOBUFS;
+        return EXIT_FAILURE;
+    }
+    buffer->buffer[buffer->head] = value;
+    buffer->head = (buffer->head + 1) % buffer->capacity;
+    if (buffer->head == buffer->tail)
+    {
+        buffer->full = 1;
+    }
+    return EXIT_SUCCESS;
+}
+
+unsigned int overwrite(circular_buffer_t *buffer, buffer_value_t value)
+{
+    if (!buffer->full)
+    {
+        return write(buffer, value);
+    }
+    buffer->buffer[buffer->head] = value;
+    buffer->head = (buffer->head + 1) % buffer->capacity;
+    buffer->tail = (buffer->tail + 1) % buffer->capacity;
+    return EXIT_SUCCESS;
+}
+
+unsigned int read(circular_buffer_t *buffer, buffer_value_t *value)
+{
+    if ((buffer->head == buffer->tail) && !buffer->full)
+    {
+        errno = ENODATA;
+        return EXIT_FAILURE;
+    }
+    *value = buffer->buffer[buffer->tail];
+    buffer->tail = (buffer->tail + 1) % buffer->capacity;
+    buffer->full = 0;
+    return EXIT_SUCCESS;
+}
--- c/circular-buffer/circular_buffer.h
+++ c/circular-buffer/circular_buffer.h
@@ -9,6 +9,14 @@ typedef struct
     int capacity;
     int head;
     int tail;
+    int full;
 } circular_buffer_t;
+
+circular_buffer_t *new_circular_buffer(int capacity);
+void clear_buffer(circular_buffer_t *buffer);
+void delete_buffer(circular_buffer_t *buffer);
+unsigned int write(circular_buffer_t *buffer, buffer_value_t value);
+unsigned int overwrite(circular_buffer_t *buffer, buffer_value_t value);
+unsigned int read(circular_buffer_t *buffer, buffer_value_t *value);
 
 #endif
