56ad54dab43fa159e311e560c09a612ca79389db
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-17 17:53:39 -0500

    Solve exercise

--- scala/circular-buffer/src/main/scala/CircularBuffer.scala
+++ scala/circular-buffer/src/main/scala/CircularBuffer.scala
@@ -3,12 +3,32 @@ class EmptyBufferException() extends Exception {}
 class FullBufferException() extends Exception {}
 
 class CircularBuffer(var capacity: Int) {
+    private var buffer: List[Int] = List()
 
-  def write(value: Int) = ???
+    def write(value: Int) = {
+        if (capacity == buffer.length) {
+            throw new FullBufferException()
+        }
+        buffer = buffer :+ value
+    }
 
-  def read(): Int = ???
+    def read(): Int = {
+        if (buffer.isEmpty) {
+            throw new EmptyBufferException()
+        }
+        val value = buffer.head
+        buffer = buffer.tail
+        value
+    }
 
-  def overwrite(value: Int) = ???
+    def overwrite(value: Int) = {
+        if (capacity == buffer.length) {
+            read()
+        }
+        write(value)
+    }
 
-  def clear() = ???
+    def clear() = {
+        buffer = List()
+    }
 }
