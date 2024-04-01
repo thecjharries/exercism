201b81d1aa4dc77e9648326c5181bebddd62cf26
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-01 10:19:30 -0500

    Solve exercise

--- bash/sieve/sieve.sh
+++ bash/sieve/sieve.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+(($# == 1)) || exit 1
+
+declare -i n=$1
+declare -a primes out
+
+for ((i = 2; i <= n; i++)); do
+    primes[i]=1
+done
+
+for ((i = 2; i <= n; i++)); do
+    ((primes[i])) || continue
+    out+=("$i")
+    for ((j = 2 * i; j <= n; j += i)); do
+        primes[j]=0
+    done
+done
+
+echo "${out[@]}"
