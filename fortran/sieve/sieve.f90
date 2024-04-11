cb75ad6d678af2fb6a6e69ad9098bbccdd705731
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-11 18:29:57 -0500

    Solve exercise

--- fortran/sieve/sieve.f90
+++ fortran/sieve/sieve.f90
@@ -6,8 +6,20 @@ contains
   function primes(limit) result(array)
     integer, intent(in) :: limit
     integer, allocatable :: array(:)
+    logical :: sieve(limit)
+    integer :: i, j
 
-    array = [limit] ! Replace this line with your implementation
+    sieve = .true.
+
+    do i = 2, int(sqrt(real(limit)))
+      if (sieve(i)) then
+        do j = i**2, limit, i
+          sieve(j) = .false.
+        end do
+      end if
+    end do
+
+    array = pack([(i, i = 2, limit)], sieve(2:))
   end function primes
 
 end module sieve
