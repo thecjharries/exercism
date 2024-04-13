5f35467ddefb6f160a66332ab5dad5275236eb11
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-13 10:16:41 -0500

    Solve exercise

--- perl5/luhn/lib/Luhn.pm
+++ perl5/luhn/lib/Luhn.pm
@@ -6,5 +6,13 @@ use Exporter qw<import>;
 our @EXPORT_OK = qw<is_luhn_valid>;
 
 sub is_luhn_valid ($number) {
-    return undef;
+    $number =~ s/\s//g;
+    return 0 if $number !~ /^\d{2,}$/;
+    my $sum = 0;
+    my $index = 1;
+    for my $digit (reverse split //, $number) {
+        $sum += $index % 2 ? $digit : $digit * 2 > 9 ? $digit * 2 - 9 : $digit * 2;
+        $index++;
+    }
+    return $sum % 10 == 0;
 }
