69c35feaef65607845816e83ac3eae0f66d78fef
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-20 19:39:50 -0500

    Solve exercise

--- scheme/scrabble-score/scrabble-score.scm
+++ scheme/scrabble-score/scrabble-score.scm
@@ -1,5 +1,16 @@
 (import (rnrs))
 
+(define (letter-score letter)
+    (case (char-upcase letter)
+      ((#\A #\E #\I #\O #\U #\L #\N #\R #\S #\T) 1)
+      ((#\D #\G) 2)
+      ((#\B #\C #\M #\P) 3)
+      ((#\F #\H #\V #\W #\Y) 4)
+      ((#\K) 5)
+      ((#\J #\X) 8)
+      ((#\Q #\Z) 10)
+      (else 0)))
+
 (define (score word)
-  'implement-me!)
+    (apply + (map letter-score (string->list word))))
 
