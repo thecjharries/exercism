d191d7750e0952bf530d861eac26052bc2044d27
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-21 18:05:09 -0500

    Solve exercise

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -1,5 +1,12 @@
-# this is a stub function that takes a natural_number
-# and should return the difference-of-squares as described
-# in the README.md
+sum_of_squares <- function(n) {
+  n * (n + 1) * (2 * n + 1) / 6
+}
+
+square_of_sum <- function(n) {
+  sum <- n * (n + 1) / 2
+  sum * sum
+}
+
 difference_of_squares <- function(natural_number) {
+  square_of_sum(natural_number) - sum_of_squares(natural_number)
 }
