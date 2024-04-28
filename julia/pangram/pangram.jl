0840a6ceca103bbd0270ad6f2373da209194b0a3
CJ Harries <cj@wizardsoftheweb.pro>
2024-04-28 16:07:21 -0500

    Solve exercise

--- julia/pangram/pangram.jl
+++ julia/pangram/pangram.jl
@@ -5,6 +5,7 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 
 """
 function ispangram(input)
-
+    alphabet = Set('a':'z')
+    return alphabet ⊆ Set(lowercase(input))
 end
 
