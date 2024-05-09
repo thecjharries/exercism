cba1a6605a9471ae3375d556449c52d2f5092785
CJ Harries <cj@wizardsoftheweb.pro>
2024-05-09 07:56:23 -0500

    Solve exercise

--- groovy/all-your-base/src/main/groovy/AllYourBase.groovy
+++ groovy/all-your-base/src/main/groovy/AllYourBase.groovy
@@ -1,10 +1,28 @@
 class AllYourBase {
 
+    private decimal = 0
+
     AllYourBase(inputBase, digits) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        if (2 > inputBase)
+            throw new ArithmeticException("Invalid base")
+        digits.each { digit ->
+            if (!(0..<inputBase).contains(digit))
+                throw new ArithmeticException("Invalid digit")
+            decimal = decimal * inputBase + digit
+        }
     }
 
     def rebase(outputBase) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        if (2 > outputBase)
+            throw new ArithmeticException("Invalid base")
+        if (0 === decimal)
+            return [0]
+        def digits = []
+        def reduction = decimal
+        while (0 < reduction) {
+            digits.push(reduction % outputBase)
+            reduction = reduction.intdiv(outputBase)
+        }
+        digits
     }
 }
