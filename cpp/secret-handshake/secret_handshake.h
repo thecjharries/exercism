0b722862418ffef6e2244bdd3b58625fdfe2ff25
CJ Harries <cj@wizardsoftheweb.pro>
2024-07-21 20:26:31 -0500

    Solve exercise

--- cpp/secret-handshake/secret_handshake.h
+++ cpp/secret-handshake/secret_handshake.h
@@ -1,8 +1,13 @@
+#pragma once
 #if !defined(SECRET_HANDSHAKE_H)
 #define SECRET_HANDSHAKE_H
 
-namespace secret_handshake {
+#include <string>
+#include <vector>
 
-}  // namespace secret_handshake
+namespace secret_handshake
+{
+    std::vector<std::string> commands(int);
+} // namespace secret_handshake
 
 #endif // SECRET_HANDSHAKE_H
