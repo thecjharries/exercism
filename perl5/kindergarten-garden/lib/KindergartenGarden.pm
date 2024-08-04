504868819cf3831ef5d3c95b6f2e20d5a6550e34
CJ Harries <cj@wizardsoftheweb.pro>
2024-08-03 19:36:16 -0500

    Create perl5:kindergarten-garden exercise

---
+++ perl5/kindergarten-garden/lib/KindergartenGarden.pm
@@ -0,0 +1,19 @@
+package KindergartenGarden;
+
+use v5.40;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<plants>;
+
+sub plants ( $diagram, $student ) {
+    my %plants = (
+        C => 'clover',
+        G => 'grass',
+        R => 'radishes',
+        V => 'violets',
+    );
+
+    return undef;
+}
+
+1;
