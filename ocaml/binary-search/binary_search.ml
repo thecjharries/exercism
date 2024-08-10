624e69a72a54f0ad3a22b9dbc66f91044f51f3fc
CJ Harries <cj@wizardsoftheweb.pro>
2024-08-10 14:35:03 -0500

    Solve exercise

--- ocaml/binary-search/binary_search.ml
+++ ocaml/binary-search/binary_search.ml
@@ -1,2 +1,13 @@
-let find _ _ =
-    failwith "'find' is missing"
+
+let find input target =
+    let rec search left right =
+      if left > right then
+        Error "value not in array"
+      else
+        let mid = (left + right) / 2 in
+        match input.(mid) with
+        | x when x = target -> Ok mid
+        | x when x < target -> search (mid + 1) right
+        | _ -> search left (mid - 1)
+    in
+    search 0 (Array.length input - 1)
