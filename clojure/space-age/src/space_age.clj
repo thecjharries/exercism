f2a4fcc66afda63974fde9ef6501eb63768c4309
CJ Harries <cj@wizardsoftheweb.pro>
2024-08-11 17:21:54 -0500

    Solve exercise

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,33 +1,33 @@
 (ns space-age)
 
-(defn on-mercury [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-earth [seconds]
+    (/ seconds 31557600)
 )
 
-(defn on-venus [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-mercury [seconds]
+    (/ (on-earth seconds) 0.2408467)
 )
 
-(defn on-earth [] ;; <- arglist goes here
-      ;; your code goes here
+(defn on-venus [seconds]
+    (/ (on-earth seconds) 0.61519726)
 )
 
-(defn on-mars [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-mars [seconds]
+    (/ (on-earth seconds) 1.8808158)
 )
 
-(defn on-jupiter [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-jupiter [seconds]
+    (/ (on-earth seconds) 11.862615)
 )
 
-(defn on-saturn [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-saturn [seconds]
+    (/ (on-earth seconds) 29.447498)
 )
 
-(defn on-uranus [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-uranus [seconds]
+    (/ (on-earth seconds) 84.016846)
 )
 
-(defn on-neptune [] ;; <- arglist goes here
-  ;; your code goes here
+(defn on-neptune [seconds]
+    (/ (on-earth seconds) 164.79132)
 )
