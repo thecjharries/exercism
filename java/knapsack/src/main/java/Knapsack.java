77d2d3dce1c91c879591a2a0f1f149dd4020fe43
CJ Harries <cj@wizardsoftheweb.pro>
2024-08-18 18:43:03 -0500

    Create java:knapsack exercise

---
+++ java/knapsack/src/main/java/Knapsack.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class Knapsack {
+
+    int maximumValue(int maximumWeight, List<Item> items) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
