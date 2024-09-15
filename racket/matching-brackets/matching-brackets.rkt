d0e1eaf5412c3e533b03880e5d6f9ef6c9c5fd0a
CJ Harries <cj@wizardsoftheweb.pro>
2024-09-15 09:52:34 -0500

    Solve exercise

--- racket/matching-brackets/matching-brackets.rkt
+++ racket/matching-brackets/matching-brackets.rkt
@@ -3,4 +3,16 @@
 (provide balanced?)
 
 (define (balanced? str)
-  (error "Not implemented yet"))
+    (define (is-balanced? lst openers)
+        (if (empty? lst)
+            (empty? openers)
+            (let ([c (car lst)] [rest (cdr lst)])
+                (match c
+                    [#\( (is-balanced? rest (cons #\) openers))]
+                    [#\{ (is-balanced? rest (cons #\} openers))]
+                    [#\[ (is-balanced? rest (cons #\] openers))]
+                    [( or #\) #\} #\]) (and (not (empty? openers))
+                                            (equal? (car openers) c)
+                                            (is-balanced? rest (cdr openers)))]
+                    [_ (is-balanced? rest openers)]))))
+    (is-balanced? (string->list str) '()))
