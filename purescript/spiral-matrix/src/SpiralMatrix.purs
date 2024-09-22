ad84c135499ee5a8ac57475dff64722ca2be791f
CJ Harries <cj@wizardsoftheweb.pro>
2024-09-22 16:50:06 -0500

    Solve exercise

--- purescript/spiral-matrix/src/SpiralMatrix.purs
+++ purescript/spiral-matrix/src/SpiralMatrix.purs
@@ -2,8 +2,28 @@ module Spiral
   ( spiral
   ) where
 
-import Data.List (List)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.List (
+    (:)
+    , drop
+    , fromFoldable
+    , List(Nil)
+    , range
+    , reverse
+    , take
+    , transpose
+    )
+import Data.Tuple (Tuple(Tuple))
 
 spiral :: Int -> List (List Int)
-spiral = unsafeThrow "You need to implement this function."
+spiral n = rectangle (range 1 (n * n)) n n
+
+rectangle :: forall a. List a -> Int -> Int -> List (List a)
+rectangle _ _ 0 = Nil
+rectangle x j k = y : rotate (rectangle z k $ j - 1) where Tuple y z = split k x
+
+rotate :: forall a. List (List a) -> List (List a)
+rotate = transpose <<< reverse
+
+split :: forall a. Int -> List a -> Tuple (List a) (List a)
+split n x = Tuple (take n x) (drop n x)
