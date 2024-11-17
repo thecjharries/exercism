cca24f09edde143ad6c25cd354f86ed31db82da3
CJ Harries <cj@wizardsoftheweb.pro>
2024-11-17 12:07:13 -0600

    Solve exercise

--- elm/rna-transcription/src/RnaTranscription.elm
+++ elm/rna-transcription/src/RnaTranscription.elm
@@ -1,6 +1,19 @@
 module RnaTranscription exposing (toRNA)
 
+import Dict
+
+complement : Dict.Dict Char Char
+complement = Dict.fromList
+    [ ( 'G', 'C' )
+    , ( 'C', 'G' )
+    , ( 'T', 'A' )
+    , ( 'A', 'U' )
+    ]
+
 
 toRNA : String -> Result String String
-toRNA dna =
-    Debug.todo "Please implement this function"
+toRNA =
+    String.toList
+        >> List.map (\c -> Dict.get c complement)
+        >> List.foldr (Maybe.map2 String.cons) (Just "")
+        >> Result.fromMaybe "Invalid input"
