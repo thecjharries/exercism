bbe8d8be4ba61ce6956b302d4bc98da7dc33c885
CJ Harries <cj@wizardsoftheweb.pro>
2024-11-26 21:33:44 -0600

    Solve exercise

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,18 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Number must be positive");
+        }
+
+        int steps = 0;
+        while (number != 1)
+        {
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+            steps++;
+        }
+
+        return steps;
     }
 }
