7d019d97b1f6baa7cfd6ac5cac1fbee27e83a5af
CJ Harries <cj@wizardsoftheweb.pro>
2024-11-28 19:09:06 -0600

    Solve exercise

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,7 +1,16 @@
-=begin
-Write your code for the 'Bob' exercise in this file. Make the tests in
-`bob_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/bob` directory.
-=end
+class Bob
+    def self.hey(s)
+        @s = s.strip
+        if (@s == @s.upcase) && @s[-1] == '?' && s.count("a-zA-Z") > 0
+            return "Calm down, I know what I'm doing!"
+        elsif @s == @s.upcase && s.count("a-zA-Z") > 0
+            return "Whoa, chill out!"
+        elsif @s[-1] == '?'
+            return "Sure."
+        elsif @s =~ /\A\s*\Z/
+            return "Fine. Be that way!"
+        else
+            return "Whatever."
+        end
+    end
+end
