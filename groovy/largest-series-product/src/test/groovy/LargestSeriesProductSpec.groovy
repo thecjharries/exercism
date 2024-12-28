ec9be707ab78fff8d368b498209761ea1ff5c46e
CJ Harries <cj@wizardsoftheweb.pro>
2024-12-28 09:01:42 -0600

    Enable all tests

--- groovy/largest-series-product/src/test/groovy/LargestSeriesProductSpec.groovy
+++ groovy/largest-series-product/src/test/groovy/LargestSeriesProductSpec.groovy
@@ -10,7 +10,6 @@ class LargestSeriesProductSpec extends Specification {
         "29"   | 2    || 18
     }
 
-    @Ignore
     def "can find the largest product of 2 with numbers in order"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -20,7 +19,6 @@ class LargestSeriesProductSpec extends Specification {
         "0123456789" | 2    || 72
     }
 
-    @Ignore
     def "can find the largest product of 2"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -30,7 +28,6 @@ class LargestSeriesProductSpec extends Specification {
         "576802143" | 2    || 48
     }
 
-    @Ignore
     def "can find the largest product of 3 with numbers in order"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -40,7 +37,6 @@ class LargestSeriesProductSpec extends Specification {
         "0123456789" | 3    || 504
     }
 
-    @Ignore
     def "can find the largest product of 3"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -50,7 +46,6 @@ class LargestSeriesProductSpec extends Specification {
         "1027839564" | 3    || 270
     }
 
-    @Ignore
     def "can find the largest product of 5 with numbers in order"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -60,7 +55,6 @@ class LargestSeriesProductSpec extends Specification {
         "0123456789" | 5    || 15120
     }
 
-    @Ignore
     def "can get the largest product of a big number"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -70,7 +64,6 @@ class LargestSeriesProductSpec extends Specification {
         "73167176531330624919225119674426574742355349194934" | 6    || 23520
     }
 
-    @Ignore
     def "reports zero if the only digits are zero"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -80,7 +73,6 @@ class LargestSeriesProductSpec extends Specification {
         "0000" | 2    || 0
     }
 
-    @Ignore
     def "reports zero if all spans include zero"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -90,7 +82,6 @@ class LargestSeriesProductSpec extends Specification {
         "99099" | 3    || 0
     }
 
-    @Ignore
     def "rejects span longer than string length"() {
         when:
         LargestSeriesProduct.largestProduct(digits, span)
@@ -104,7 +95,6 @@ class LargestSeriesProductSpec extends Specification {
         "123"  | 4    || "span must be smaller than string length"
     }
 
-    @Ignore
     def "reports 1 for empty string and empty product (0 span)"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -114,7 +104,6 @@ class LargestSeriesProductSpec extends Specification {
         ""     | 0    || 1
     }
 
-    @Ignore
     def "reports 1 for nonempty string and empty product (0 span)"() {
         expect:
         LargestSeriesProduct.largestProduct(digits, span) == expectedResult
@@ -124,7 +113,6 @@ class LargestSeriesProductSpec extends Specification {
         "123"  | 0    || 1
     }
 
-    @Ignore
     def "rejects empty string and nonzero span"() {
         when:
         LargestSeriesProduct.largestProduct(digits, span)
@@ -138,7 +126,6 @@ class LargestSeriesProductSpec extends Specification {
         ""     | 1    || "span must be smaller than string length"
     }
 
-    @Ignore
     def "rejects invalid character in digits"() {
         when:
         LargestSeriesProduct.largestProduct(digits, span)
@@ -152,7 +139,6 @@ class LargestSeriesProductSpec extends Specification {
         "1234a5" | 2    || "digits input must only contain digits"
     }
 
-    @Ignore
     def "rejects negative span"() {
         when:
         LargestSeriesProduct.largestProduct(digits, span)
