66f0f323a9ba86f3d20c08fd369bdd8a439d58b6
CJ Harries <cj@wizardsoftheweb.pro>
2024-12-28 09:19:34 -0600

    Solve exercise

--- groovy/largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
+++ groovy/largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
@@ -1,5 +1,23 @@
 class LargestSeriesProduct {
-    static largestProduct(digits, span) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    private static final Closure<Integer> PRODUCT = {
+        it.inject(1) { acc, i -> acc * i }
+    }
+    static largestProduct(String digits, int span) {
+        if (span < 0) {
+            throw new IllegalArgumentException("span must not be negative")
+        }
+        if (span > digits.size()) {
+            throw new IllegalArgumentException("span must be smaller than string length")
+        }
+        if (span == 0) {
+            return 1
+        }
+        if (digits =~ /\D/) {
+            throw new IllegalArgumentException("digits input must only contain digits")
+        }
+        digits.collect { it as int }
+              .collate(span, 1, false)
+              .collect(PRODUCT)
+              .max()
     }
 }
