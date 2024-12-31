70e9d54e192b51c1f495877b64429fda12318b03
CJ Harries <cj@wizardsoftheweb.pro>
2024-12-31 17:54:08 -0600

    Enable all tests

--- typescript/collatz-conjecture/collatz-conjecture.test.ts
+++ typescript/collatz-conjecture/collatz-conjecture.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, xit } from '@jest/globals'
+import { describe, it, expect, it } from '@jest/globals'
 import { steps } from './collatz-conjecture.ts'
 
 describe('CollatzConjecture', () => {
@@ -7,36 +7,36 @@ describe('CollatzConjecture', () => {
     expect(steps(1)).toBe(expected)
   })
 
-  xit('divide if even', () => {
+  it('divide if even', () => {
     const expected = 4
     expect(steps(16)).toBe(expected)
   })
 
-  xit('even and odd steps', () => {
+  it('even and odd steps', () => {
     const expected = 9
     expect(steps(12)).toBe(expected)
   })
 
-  xit('Large number of even and odd steps', () => {
+  it('Large number of even and odd steps', () => {
     const expected = 152
     expect(steps(1000000)).toBe(expected)
   })
 
-  xit('zero is an error', () => {
+  it('zero is an error', () => {
     const expected = 'Only positive integers are allowed'
     expect(() => {
       steps(0)
     }).toThrow(expected)
   })
 
-  xit('negative value is an error', () => {
+  it('negative value is an error', () => {
     const expected = 'Only positive integers are allowed'
     expect(() => {
       steps(-15)
     }).toThrow(expected)
   })
 
-  xit('non-integer value is an error', () => {
+  it('non-integer value is an error', () => {
     const expected = 'Only positive integers are allowed'
     expect(() => {
       steps(3.1415)
